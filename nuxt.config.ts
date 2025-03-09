7619f631ab258ab286c06a82b2fa169235c0bca3
silverbackdan <daniel@silverback.is>
2025-03-09 20:00:42 +0000

    Complete Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,30 @@ export default defineNuxtConfig({
       ]
     }
   },
+  auth: {
+    globalAppMiddleware: true,
+    baseURL: '/api/auth',
+    provider: {
+      type: 'local',
+      pages: {
+        login: '/sign-in'
+      },
+      endpoints: {
+        signIn: { path: '/login', method: 'post' },
+        signOut: { path: '/logout', method: 'post' },
+        signUp: { path: '/register', method: 'post' },
+        getSession: { path: '/session', method: 'get' },
+      },
+      token: {
+        signInResponseTokenPointer: '/token',
+        cookieName: 'auth.token',
+        type: 'Bearer',
+        headerName: 'Authorization',
+        maxAgeInSeconds: 3600 * 124, // 120 hours
+        sameSiteAttribute: 'lax'
+      }
+    }
+  },
   devtools: { enabled: true },
   runtimeConfig: {
     varnishToken: '',
@@ -30,6 +54,34 @@ export default defineNuxtConfig({
     }
   },
   css: ['~/assets/css/main.css'],
+  googleFonts: {
+    families: {
+      Lato: [400, 700]
+    }
+  },
+  gtag: {
+    enabled: true,
+    id: 'GTM-5Q3VQ68',
+    initCommands: [
+      // Setup up consent mode
+      ['consent', 'default', {
+        ad_user_data: 'granted',
+        ad_personalization: 'granted',
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+        wait_for_update: 500,
+      }]
+    ]
+  },
+  icon: {
+    serverBundle: {
+      collections: ['heroicons-outline', 'heroicons-solid', 'flowbite', 'emojione-monotone', 'fa', 'material-symbols']
+    }
+  },
+  lodash: {
+    prefix: "_",
+    upperAfterPrefix: false
+  },
   modules: [
     '@vite-pwa/nuxt',
     'nuxt-gtag',
@@ -41,6 +93,39 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     'nuxt-svgo'
   ],
+  pwa: {
+    manifest: {
+      name: 'Paper Authoring Tool',
+      description: 'A tool to author RCT study papers',
+      lang: 'en',
+      short_name: 'PAT',
+      theme_color: '#22AC2E',
+      background_color: '#FFFFFF',
+      icons: [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
