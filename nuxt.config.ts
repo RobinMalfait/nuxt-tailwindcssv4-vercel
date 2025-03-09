728ca40e6a81de23714deb34196f3b3087d6a4aa
silverbackdan <daniel@silverback.is>
2025-03-09 19:57:51 +0000

    Enable modules that may conflict in transformation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,44 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      meta: [
+        {charset: 'utf-8'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {
+          name: 'description',
+          content: 'Paper Authoring Tool'
+        }
+      ],
+      link: [
+        {
+          rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
+  runtimeConfig: {
+    varnishToken: '',
+    public: {
+      apiUrl: '',
+      apiUrlBrowser: '',
+      papersEndpoint: '/account/papers'
+    }
+  },
   css: ['~/assets/css/main.css'],
+  modules: [
+    '@vite-pwa/nuxt',
+    'nuxt-gtag',
+    '@nuxtjs/google-fonts',
+    '@sidebase/nuxt-auth',
+    'nuxt-auth-utils',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    '@nuxt/icon',
+    'nuxt-svgo'
+  ],
   vite: {
     plugins: [
       tailwindcss(),
