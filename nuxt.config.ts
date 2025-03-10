d8b6e4ff19eafe76896e577fca612cfe556f99fc
silverbackdan <daniel@silverback.is>
2025-03-10 11:45:48 +0000

    No need for subdir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  css: ['~/assets/css/tailwind.css'],
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   vite: {
     plugins: [
